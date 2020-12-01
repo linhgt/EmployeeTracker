a283f1b63cad07e8cf04e664a34b9b67204a714f
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-01 10:35:24 +0700

    Add actions to the menu

--- index.js
+++ index.js
@@ -7,10 +7,50 @@ var connection = mysql.createConnection({
     port: "3306",
     user: "root",
     password: "Hoanhtrang@123",
-    database: "EmployeeTrackerDB"
+    database: "employeetrackerdb"
 });
 
 connection.connect(function(err){
     if(err) throw err;
-    console.log("successfully making connection to mysql database!");
+    start();
 });
+
+function start(){
+    inquirer.prompt([
+        {
+            type:"list",
+            message:"What would you like to do?",
+            name:"action",
+            choices:[
+                        "View employee", 
+                        "View department", 
+                        "View role", 
+                        "Add employee", 
+                        "Add department", 
+                        "Add role", 
+                        "Update roles",
+                        "Exit"
+                    ]
+        }
+    ]).then(answer => {
+            switch(answer.action){
+                case "View employee":
+                    break;
+                case "View department":
+                    break;
+                case "View role":
+                    break;
+                case "Add employee":
+                    break;
+                case "Add department":
+                    break;
+                case "Add role":
+                    break;
+                case "Update role":
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+    });
+};

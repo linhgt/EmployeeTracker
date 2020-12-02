bd3481769a6ae4b5fcf2e57cf5f2e65ae2c0d11f
Linh Nguyen <linhnhatmen@gmail.com>
2020-12-02 16:23:15 +0700

    implement adding employee into the database

--- index.js
+++ index.js
@@ -88,19 +88,110 @@ function addEmployee(){
         },
         {
             //Role
-            type:"input",
+            type:"list",
             name:"role",
-            message:"Role of the employee:"
+            message:"Role of the employee:",
+            choices:[
+                "Sales Lead",
+                "Saleperson",
+                "Lead Engineer",
+                "Software Engineer",
+                "Accountant",
+                "Legal Team Lead",
+                "Lawyer"
+            ]
         },
         {
             //Manager
             type:"input",
             name:"manager",
-            message:"Manager of the employee:",
-            when: (answer) => answer.role !== 'manager'
+            message:"Manager of the employee:"
         }
     ]).then(answer => {
-        console.log("test");
+        let roleId = 0;         //Store retrieved roleId
+        let managerId = 0;
+
+        //Query retrieving role id from the roles table
+        //Based on input role
+        let roleQuery = "SELECT id FROM roles WHERE title = ?;";
+        connection.query(roleQuery, [answer.role], function(err,res){
+            if (err){
+                throw err;
+            }
+            roleId = res[0].id;
+            //function to retrieve managerId
+            findManager(answer.firstName, answer.lastName, roleId, answer.manager);
+        });
+
+        /*//Retrieve Manager Id if there is a manager
+        if(answer.manager !== "")
+        {
+            let managerArr = answer.manager.split(" ");
+            let managerQuery = "SELECT id FROM employee WHERE first_name = ? AND last_name = ?";
+            connection.query(managerQuery, [managerArr[0], managerArr[1]], function(err, res){
+                if (err) throw err;
+                managerId = res[0].id;
+            });
+        }
+        
+        //Insert query
+        let employeeQuery = "INSERT INTO employee SET ?";
+        connection.query(employeeQuery,
+            {
+                first_name: answer.firstName,
+                last_name: answer.lastName,
+                role_id:roleId,
+                manager_id:managerId   
+            }, 
+            function(err, res){
+            if (err) throw err;
+            console.log(roleId + "\n");
+            console.log("Successfully inserted!");
+        });
+        start();*/
+    });
+}
+
+//Function to retrieve managerId
+function findManager(firstName, lastName, roleId, manager){
+    let managerId;
+
+    //If there is a manager
+    if(manager !== "")
+    {
+        //Split manager name into first name and last name
+        let managerArr = manager.split(" ");
+        let managerQuery = "SELECT id FROM employye WHERE first_name = ? AND last_name = ?;";
+        connection.query(managerQuery, [managerArr[0], managerArr[1]], function(err, res){
+            if (err){
+                throw err;
+            }
+            //Pass managerId to insert function
+            managerId = res[0].id;
+            insertEmployee(firstName, lastName, roleId, managerId);
+        });
+    }
+    else
+    {
+        //Insert employee without managerId
+        insertEmployee(firstName, lastName, roleId, 0);
+    }
+};
+
+//Insert employee into the database
+function insertEmployee(firstName, lastName, roleId, managerId)
+{
+    //Pass null in managerId column if there is no manager
+    if(managerId ==0)
+    {
+        managerId = null;
+    }
+    let insertQuery = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);";
+    connection.query(insertQuery, [firstName, lastName, roleId, managerId], function(err,res){
+        if (err){
+            throw err;
+        }
+        console.log("Successfully inserted!");
         start();
     });
 }
